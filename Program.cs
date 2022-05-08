19d596a2f5041d25071b4251fcd2bd0d3a0be215
Tanguy Baltazart <tbalta914@gmail.com>
2022-05-08 17:57:58 +0200

    base client.
    Co-authored-by: Nicolas Kourban <NicKOOU@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Text.Json;
 
 using System.Net.Http;
@@ -8,23 +9,25 @@ namespace HackOfLegend
 {
     class Program
     {
+        private static HttpClient client = new HttpClient();
+
         struct champ_select
         {
-            public long? gameId {get; set;}
-            public int? httpStatus {get; set;}
+            public long? gameId { get; set; }
+            public int? httpStatus { get; set; }
             public struct my_team
             {
-                public string assignedPosition {get; set;}
+                public string assignedPosition { get; set; }
             }
-            public List<my_team> myTeam {get; set;}
+            public List<my_team> myTeam { get; set; }
         }
         struct champ_info
         {
-            public int? champion_id {get; set;}
+            public int? champion_id { get; set; }
         }
 
 
-        static void wait_for_champ_select(Lcu lcu)
+        static champ_select wait_for_champ_select(Lcu lcu)
         {
             champ_select select = JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session"));
             string test = lcu.get("/lol-champ-select/v1/session");
@@ -34,36 +37,46 @@ namespace HackOfLegend
                 System.Threading.Thread.Sleep(1000);
                 select = JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session"));
             }
-
+            return select;
         }
 
-        static void select_champ(Lcu lcu)
+        static void select_champ(Lcu lcu, string assignedPosition)
         {
 
             String champ = lcu.get("/lol-champ-select/v1/current-champion");
             while (champ == "0")
             {
                 Console.WriteLine("Waiting for a champion...");
-                System.Threading.Thread.Sleep(2000);
+                System.Threading.Thread.Sleep(1000);
                 champ = lcu.get("/lol-champ-select/v1/current-champion");
+
             }
+            if(assignedPosition == "")
+                assignedPosition = "MID";
+            client.GetAsync($"/runes/{champ}/{assignedPosition}").Result.Content.ReadAsStringAsync();
             Console.WriteLine("Champion selected!");
             Console.WriteLine(champ);
+
+        }
+
+        static void logic(Lcu lcu)
+        {
+            var champ_select = wait_for_champ_select(lcu);
+            select_champ(lcu, champ_select.myTeam[0].assignedPosition);
         }
 
 
         static void Main(string[] args)
         {
+            client.BaseAddress = new Uri("https://localhost:8080");
             var lcu = new Lcu("C:\\Riot Games\\League of Legends\\lockfile");
             Console.WriteLine(lcu);
-            Console.WriteLine(lcu.get("/lol-perks/v1/currentpage"));
             wait_for_champ_select(lcu);
             string json = lcu.get("/lol-perks/v1/currentpage");
             Rune rune = JsonSerializer.Deserialize<Rune>(json);
             rune.name = "salut c pas moi";
             lcu.put("/lol-perks/v1/pages/" + rune.id.ToString(), rune.ToString());
             Console.WriteLine(rune);
-            select_champ(lcu);
             String champ = lcu.get("/lol-champ-select/v1/current-champion");
             champ_select.my_team myTeam = JsonSerializer.Deserialize<champ_select.my_team>(lcu.get("/lol-champ-select/v1/session"));
             Console.WriteLine(myTeam.assignedPosition);
