e4e74364027315fcdd0a4d634f6a5eb43207d97e
Nicolas KOURBAN <nicol@LAPTOP-1E90D33L>
2022-05-11 14:57:27 +0200

    reverse tanguy marche

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace HackOfLegend
             public struct my_team
             {
                 public string assignedPosition { get; set; }
-                
+
             }
             public List<my_team> myTeam { get; set; }
         }
@@ -45,30 +45,30 @@ namespace HackOfLegend
             public string? phase { get; set; }
         }
 
-        // static champ_select wait_for_champ_select(Lcu lcu)
-        // {
-        //     champ_select select = JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session"));
-        //     string test = lcu.get("/lol-champ-select/v1/session");
-        //     Console.WriteLine(lcu.get("/lol-gameflow/v1/session"));
-        //     while (select.gameId == null)
-        //     {
-        //         Console.WriteLine("Waiting for champ select...");
-        //         System.Threading.Thread.Sleep(1000);
-        //         select = JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session"));
-        //     }
-        //     return select;
-        // }
+        static champ_select wait_for_champ_select(Lcu lcu)
+        {
+            champ_select select = JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session"));
+            string test = lcu.get("/lol-champ-select/v1/session");
+            //Console.WriteLine(lcu.get("/lol-gameflow/v1/session"));
+            while (select.gameId == null)
+            {
+                //Console.WriteLine("Waiting for champ select...");
+                System.Threading.Thread.Sleep(1000);
+                select = JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session"));
+            }
+            return select;
+        }
 
         static void select_champ(Lcu lcu, string assignedPosition)
         {
 
-            // String champ = lcu.get("/lol-champ-select/v1/current-champion");
-            // for (; champ == "0"; champ = lcu.get("/lol-champ-select/v1/current-champion"))
-            // {
-            //     Console.WriteLine("Waiting for a champion...");
-            //     System.Threading.Thread.Sleep(1000);
-            // }
-            String champ = get_champ(lcu);
+            String champ = lcu.get("/lol-champ-select/v1/current-champion");
+            for (; champ == "0"; champ = lcu.get("/lol-champ-select/v1/current-champion"))
+            {
+                //Console.WriteLine("Waiting for a champion...");
+                System.Threading.Thread.Sleep(1000);
+            }
+            //String champ = get_champ(lcu);
             if (champ[0] == '{')
                 return;
             if (assignedPosition == "")
@@ -99,16 +99,18 @@ namespace HackOfLegend
         }
         static gameflow get_gameflow(Lcu lcu)
         {
-            // gameflow gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
-            // while(gameflow.phase == "ChampSelect")
-            // {
-            //     Console.WriteLine("Waiting for game to start...");
-            //     System.Threading.Thread.Sleep(1000);
-            //     gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
-            // }
-            gameflow gameflow = wait_something<gameflow>(() => JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow")), (gameflow) =>  gameflow.phase != "ChampSelect", 1000);
+            System.Threading.Thread.Sleep(2000);
+            gameflow gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
+            Console.WriteLine("Gameflow: " + gameflow.phase);
+            while (gameflow.phase == "ChampSelect")
+            {
+                Console.WriteLine("Waiting for game to start...");
+                System.Threading.Thread.Sleep(1000);
+                gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
+            }
+            //gameflow gameflow = wait_something<gameflow>(() => JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow")), (gameflow) => gameflow.phase == "ChampSelect", 1000);
             // gameflow gameflow = wait_end_game(lcu);
-            if(gameflow.phase == "InProgress")
+            if (gameflow.phase == "InProgress")
             {
                 Console.WriteLine("Game started!");
                 return gameflow;
@@ -127,44 +129,53 @@ namespace HackOfLegend
             }
             return val;
         }
-
+        /*
         static Func<Lcu, champ_select> wait_for_champ_select = (Lcu lcu) => wait_something<champ_select>(() => JsonSerializer.Deserialize<champ_select>(lcu.get("/lol-champ-select/v1/session")), (select) => select.gameId != null, 1000);
         static Func<Lcu, gameflow> Check_End_Game = (Lcu lcu) => wait_something<gameflow>(() => JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow")), (gameflow) => gameflow.phase != "InProgress", 5000);
-        static Func<Lcu, gameflow> wait_end_game = (Lcu lcu) => wait_something<gameflow>(() => JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow")), (gameflow) => gameflow.phase == "ChampSelect", 1000);
-        static Func<Lcu, String> get_champ = (Lcu lcu) => wait_something<String>(() => (lcu.get("/lol-gameflow/v1/gameflow")), (str) => str != "0", 1000);
-
+        static Func<Lcu, gameflow> wait_end_game = (Lcu lcu) => wait_something<gameflow>(() => JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow")), (gameflow) => gameflow.phase != "ChampSelect", 1000);
+        static Func<Lcu, String> get_champ = (Lcu lcu) => wait_something<String>(() => (lcu.get("/lol-gameflow/v1/gameflow")), (str) => str == "0", 1000);
+        */
 
 
 
 
 
         //check toutes les 5 secondes si la game est fine.
-        
-        // static void Check_End_Game(Lcu lcu)
-        // {
-        //     gameflow gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
-        //     while(gameflow.phase == "InProgress")
-        //     {
-        //         Console.WriteLine("In Game");
-        //         System.Threading.Thread.Sleep(5000);
-        //         gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
-        //     }
-        // }
-        
+
+        static void Check_End_Game(Lcu lcu)
+        {
+            gameflow gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
+            while (gameflow.phase == "InProgress")
+            {
+                Console.WriteLine("In Game");
+                System.Threading.Thread.Sleep(5000);
+                gameflow = JsonSerializer.Deserialize<gameflow>(lcu.get("/lol-gameflow/v1/gameflow"));
+            }
+        }
+
         static void logic(Lcu lcu, State state)
         {
             while (true)
             {
-                state=State.Idle;
-                var champ_select = wait_for_champ_select(lcu);                    
+                state = State.Idle;
+                var champ_select = wait_for_champ_select(lcu);
+                Console.WriteLine("Champion select!");
                 select_champ(lcu, champ_select.myTeam[0].assignedPosition);
+                Console.WriteLine("Champion selected!");
                 state = State.ChampSelect;
-                gameflow gameflow=get_gameflow(lcu);
-                if (gameflow.phase=="InProgress"){
-                    state=State.InGame;
+                gameflow gameflow = get_gameflow(lcu);
+                Console.WriteLine("Game started!");
+                if (gameflow.phase == "InProgress")
+                {
+                    state = State.InGame;
                     Check_End_Game(lcu);
                     //Envoyer la GameID à l'API
-                }                 
+                }
+                else
+                {
+                    Console.WriteLine(gameflow.phase);
+                    return;
+                }
             }
         }
         static void Main(string[] args)
@@ -173,8 +184,7 @@ namespace HackOfLegend
             client.BaseAddress = new Uri("http://127.0.0.1:8080");
             var lcu = new Lcu("C:\\Riot Games\\League of Legends\\lockfile");
             Console.WriteLine(lcu);
-            wait_for_champ_select(lcu);
-            logic(lcu,gamestate);
+            logic(lcu, gamestate);
         }
     }
 }
