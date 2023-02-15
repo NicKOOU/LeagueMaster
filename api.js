bab2b386118d80b01ddc6ecc501ce776c3255692
Nicolas Kourban <nicolas.kourban@epita.fr>
2023-02-15 11:21:58 +0100

    add features

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const { default: axios } = require('axios');
 const { createClient } = require('@supabase/supabase-js');
 var fs = require('fs');
 const authenticate = require('league-connect');
+const runesjson = require('./runes.json');
 axios.baseURL = "https://yshzrbmwnmyhhbldbvqg.supabase.co";
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.apikey;
 
@@ -11,6 +12,32 @@ class DB {
         this.apikey = fs.readFileSync('apikey.txt', 'utf8');
         this.client = createClient('https://yshzrbmwnmyhhbldbvqg.supabase.co', this.apikey);
     }
+}
+
+class Rune {
+    constructor()
+    {
+        this.primarystyleid = 0;
+        this.substyleid = 0;
+        this.selectedPerkIds = [];
+        this.name = "";
+        this.current = true;
+    }
+}
+
+async function showAllRunesInConsoleToChoose(champion_id, assignedPosition)
+{
+    let database_rune = await dataBase.client.from('runes').select('*').eq('champion_id', champion_id).eq('lane', assignedPosition).order('count', 'desc');
+    // display all runes in console to choose with index
+    for(let i = 0; i < database_rune.data.length; i++)
+    {
+        console.log(i + " : " + runesjson.primary[database_rune.data[i].primarystyleid] + " " + runesjson.secondary[database_rune.data[i].substyleid] + " " + runesjson.primary[database_rune.data[i].primary1] + " " + runesjson.primary[database_rune.data[i].primary2] + " " + runesjson.primary[database_rune.data[i].primary3] + " " + runesjson.primary[database_rune.data[i].primary4]);
+    }
+    let runeIndex = await readline.question('Which runes do you want to use? : ');
+    runeIndex = parseInt(runes);
+    let newrune = new Rune(rune.name, rune.primaryStyleId, rune.subStyleId, rune.selectedPerkIds);
+    send_rune_to_lc(newrune);
+
 }
 
 function addshards()
@@ -24,6 +51,19 @@ function addshards()
     let shard3 = list3[randomnumberbettween0and2];
     return [shard1, shard2, shard3];
 }
+
+async function parseDataBaseRune(database_rune, name, index)
+{
+    let shards = addshards();
+    let text = rune._raw.toString();
+    rune = JSON.parse(text);
+    rune.name = "PJD " + name;
+    rune.primaryStyleId = database_rune.data[index].primarystyleid;
+    rune.subStyleId = database_rune.data[index].substyleid;
+    rune.selectedPerkIds = [database_rune.data[index].primary1, database_rune.data[index].primary2, database_rune.data[index].primary3, database_rune.data[index].primary4, database_rune.data[index].sub1, database_rune.data[index].sub2, shards[0], shards[1], shards[2]];
+    return rune;
+}
+
 
 async function champandrune(data, lcu)
 {
@@ -54,40 +94,36 @@ async function champandrune(data, lcu)
             stats.push(database_rune.data[i].count + database_rune.data[i].winrate);
     }
     let max = Math.max(...stats);
-    let max_index = stats.indexOf(max);
+    let index = stats.indexOf(max);
     if (database_rune.data.length == 0)
         rune.name = "Not Found";
     else
     {
-        let shards = addshards();
-        let text = rune._raw.toString();
-        rune = JSON.parse(text);
-        rune.name = "PJD " + data;
-        rune.primaryStyleId = database_rune.data[max_index].primarystyleid;
-        rune.subStyleId = database_rune.data[max_index].substyleid;
-        rune.selectedPerkIds = [database_rune.data[max_index].primary1, database_rune.data[max_index].primary2, database_rune.data[max_index].primary3, database_rune.data[max_index].primary4, database_rune.data[max_index].sub1, database_rune.data[max_index].sub2, shards[0], shards[1], shards[2]];
+        rune = await parseDataBaseRune(database_rune, assignedPosition, index);
     }
-    let newrune = {};
-    newrune.name = rune.name;
-    newrune.primaryStyleId = rune.primaryStyleId;
-    newrune.subStyleId = rune.subStyleId;
-    newrune.selectedPerkIds = rune.selectedPerkIds;
-    newrune.current = true;
+    let newrune = new Rune(rune.name, rune.primaryStyleId, rune.subStyleId, rune.selectedPerkIds);
+    send_rune_to_lc(newrune);
+    showAllRunesInConsoleToChoose(data, assignedPosition);
+}
+
+async function send_rune_to_lc(rune)
+{
     try
     {
         let del = await authenticate.createHttp1Request({
             method: 'DELETE',
             url: '/lol-perks/v1/pages'
         }, lcu.credentials)
-        let deltext = del._raw.toString();
     }
     catch (err)
     {
+
+        console.log(err);
     }
     let send = await authenticate.createHttp1Request({
         method: 'POST',
         url: '/lol-perks/v1/pages',
-        body: newrune
+        body: rune
     }, lcu.credentials);
 }
 
